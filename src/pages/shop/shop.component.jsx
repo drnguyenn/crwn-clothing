248b6db92e041a6e7211690cfef1923317a2edc0
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2020-03-17 02:00:59 +0700

    fix: mistake in shop page'state

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -8,11 +8,13 @@ class ShopPage extends Component {
   constructor(props) {
     super(props);
 
-    this.state = SHOP_DATA;
+    this.state = {
+      collections: SHOP_DATA
+    };
   }
 
   render() {
-    const collections = this.state;
+    const { collections } = this.state;
     return (
       <div className='shop-page'>
         {collections.map(({ id, ...otherCollectionProps }) => (
