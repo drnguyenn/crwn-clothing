a6eb2fb2b319bdc9b5b006a1b424e1859c672d18
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2020-06-07 23:02:20 +0700

    chore: remove redundant method in ShopActionTypes

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,10 +1,5 @@
 import ShopActionTypes from './shop.types';
 
-import {
-  firestore,
-  convertCollectionsSnapshotToMap
-} from '../../firebase/firebase.utils';
-
 export const fetchColllectionsStart = () => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_START
 });
@@ -18,16 +13,3 @@ export const fetchColllectionsFailure = errorMessage => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
   payload: errorMessage
 });
-
-export const fetchColllectionsStartAsync = () => dispatch => {
-  const collectionRef = firestore.collection('collections');
-  dispatch(fetchColllectionsStart());
-
-  collectionRef
-    .get()
-    .then(snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      dispatch(fetchColllectionsSuccess(collectionsMap));
-    })
-    .catch(error => dispatch(fetchColllectionsFailure(error.message)));
-};
