d8c7e2d2ee6ea13e4db00f4fea41c575af4c89c2
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2020-08-04 10:59:51 +0700

    feat: implement Express server to handle backend stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_N3beCR6RNa0oYDroenCf3aGI00GUMH03rH';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful !');
+    axios({
+      url: 'payment',
+      method: 'POST',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert('Payment successful.');
+      })
+      .catch(error => {
+        console.log('Payment error:', error);
+        alert(
+          'There was an issue with your payment. Please make sure to use the provided credit card.'
+        );
+      });
   };
 
   return (
